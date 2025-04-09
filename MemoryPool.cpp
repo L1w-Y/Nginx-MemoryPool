c8206a73951cf9990ec30c4e2747f11b14126b64
L1w137 <L1w2022@163.com>
2025-04-09 11:44:12 +0800

    :construction:构建内存池

--- MemoryPool.cpp
+++ MemoryPool.cpp
@@ -3,3 +3,58 @@
 //
 
 #include "MemoryPool.h"
+
+void* MemoryPool::ngx_create_pool(size_t size) {
+    ngx_pool_s* p;
+    p = static_cast<ngx_pool_s*>(malloc(size));
+    if (!p) {
+        return nullptr;
+    }
+
+    p->d.last = reinterpret_cast<u_char *>(p)+ sizeof(ngx_pool_s);
+    p->d.end = reinterpret_cast<u_char *>(p)+size;
+    p->d.failed=NULL;
+    p->d.next = nullptr;
+    p->current = p;
+    p->cleanup =nullptr;
+    p->large = nullptr;
+    p->max = (size < NGX_MAX_ALLOC_FROM_POOL) ? size : NGX_MAX_ALLOC_FROM_POOL;
+    return p;
+}
+
+void * MemoryPool::ngx_palloc(const size_t size) {
+    if (size>pool_->max) {
+        return  ngx_palloc_large(size);
+    }
+    return ngx_palloc_small(size,1);
+}
+
+void * MemoryPool::ngx_pnalloc(size_t size) {
+}
+
+void * MemoryPool::ngx_pcalloc(size_t size) {
+}
+
+void MemoryPool::ngx_destroy_pool() {
+}
+
+void MemoryPool::ngx_reset_pool() {
+}
+
+void MemoryPool::ngx_pfree(void *p) {
+}
+
+void * MemoryPool::ngx_alloc(size_t size) {
+}
+
+void * MemoryPool::ngx_calloc(size_t size) {
+}
+
+void * MemoryPool::ngx_palloc_small(size_t size, ngx_uint_t align) {
+}
+
+void * MemoryPool::ngx_palloc_large(size_t size) {
+}
+
+void * MemoryPool::ngx_palloc_block(size_t size) {
+}
--- MemoryPool.h
+++ MemoryPool.h
@@ -4,11 +4,86 @@
 
 #ifndef MEMORYPOOL_H
 #define MEMORYPOOL_H
+#include <cstdint>
+#include<functional>
 
 
+using u_char = unsigned char;
+using ngx_uint_t = unsigned int;
+struct ngx_pool_s;
+struct ngx_pool_large_s;
+
+constexpr int ngx_pagesize = 4096;
+constexpr int NGX_MAX_ALLOC_FROM_POOL=ngx_pagesize - 1;
+
+constexpr int NGX_DEFAULT_POOL_SIZE = 16 * 1024;
+
+constexpr size_t NGX_POOL_ALIGNMENT=16;  //内存对齐：16字节
+
+
+// 对整数进行字节对齐
+template <typename T>
+constexpr T ngx_align(T d, T a) noexcept {
+    return (d + (a - 1)) & ~(a - 1);
+}
+
+// 对指针进行字节对齐，返回原始类型指针
+template<typename T>
+constexpr T* align_ptr(T* ptr, size_t alignment) {
+    return reinterpret_cast<T*>(
+        (reinterpret_cast<uintptr_t>(ptr) + alignment - 1) & ~(alignment - 1)
+    );
+}
+
+using ngx_pool_cleanup_pt = std::function<void(void*)>;
+
+struct ngx_pool_cleanup_t {
+    ngx_pool_cleanup_pt   handler;
+    void                 *data;
+    ngx_pool_cleanup_t   *next;
+};
+
+
+struct ngx_pool_large_s {
+    ngx_pool_large_s     *next;
+    void                 *alloc;
+};
+
+struct ngx_pool_data_t{
+    u_char               *last;
+    u_char               *end;
+    ngx_pool_s           *next;
+    ngx_uint_t            failed;
+};
+
+//内存池头部
+struct ngx_pool_s {
+    ngx_pool_data_t       d;
+    size_t                max;
+    ngx_pool_s           *current;
+    ngx_pool_large_s     *large;
+    ngx_pool_cleanup_t   *cleanup;
+};
+
+constexpr int NGX_MIN_POOL_SIZE =ngx_align((sizeof(ngx_pool_s) + 2 * sizeof(ngx_pool_large_s)),NGX_POOL_ALIGNMENT);
 
 class MemoryPool{
+public:
+    static void* ngx_create_pool(size_t size);//创建内存池
+    void* ngx_palloc(size_t size);//内存对齐的内存分配
+    void* ngx_pnalloc(size_t size);//不考虑内存对齐的内存分配
+    void* ngx_pcalloc(size_t size);//内存对齐，初始化为0
+    void ngx_destroy_pool();//调用cleanup回调函数
+    void ngx_reset_pool();//重置内存池，先释放大块内存
+    void ngx_pfree(void *p);//释放大块内存
 
+    void* ngx_alloc(size_t size);
+    void* ngx_calloc(size_t size);
+private:
+	ngx_pool_s *pool_ = nullptr;//内存池入口指针
+    void* ngx_palloc_small(size_t size,ngx_uint_t align);
+    void* ngx_palloc_large(size_t size);
+    void* ngx_palloc_block(size_t size);
 };
 
 
